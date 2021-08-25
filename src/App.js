1d4bbdcc810d955ad95e39e58418264ddae5bd5b
Kyaah <kyaahh@gmail.com>
2021-08-25 14:46:28 -0500

    added components, css, and styled the homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 import './App.css';
+// import Portfolio from './components/Portfolio'
+import Resume from './components/Resume'
+import Contact from './components/Contact'
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav className="navbar">
+            <Link to="/">Home</Link>
+            {/* <Link to="/portfolio">Portfolio</Link> */}
+            <Link to="/contact">Contact</Link>
+            <Link to="/resume">Resume</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            {/* <Route path="/portfolio">
+              <Portfolio />
+            </Route> */}
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/resume">
+              <Resume />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+
+        <div>
+            <h1>About Me</h1>
+            <img 
+      src="https://media-exp1.licdn.com/dms/image/C4D03AQHqFH28hhBoGw/profile-displayphoto-shrink_800_800/0/1565212528491?e=1635379200&v=beta&t=-BXzWLVr2JAGrkXLeZ8Cc54wYl7NiQh7kk_C_zwZG1o"
+      alt="new"
+      />      
+
+      <p id="about-me">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  
+      </div>
+    )
+}
