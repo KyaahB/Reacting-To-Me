69c930500cfecdb9c28b424775f354b87133adc9
Kyaah <kyaahh@gmail.com>
2021-08-25 15:31:19 -0500

    added input fields, a button and headers to the contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,36 @@
-export default function Contact(){
-    return <h1>Contact</h1>
+export default function Contact() {
+    return (
+        <div className="contact-div">
+            <h1>Contact</h1>
+
+            <form>
+             <h2>Name</h2>
+                <input
+                 type="text"
+
+                />
+
+            </form>
+             <h2>Email</h2>
+                <input
+                 type="text"
+            />
+
+            <form>
+             <h2>Message</h2>
+                <input id="message-input"
+                 type="text"
+                />
+            </form>
+
+            <form>
+                <input id="submit-btn"
+                 type="submit"
+                 value="Send Msg"
+                // onClick={handleSubmit}
+                />
+            </form>
+
+        </div>
+    )
 }
